c00a9194f6639e9af09a41372de78eddcdac9764
deniscuni01 <denisckck25@gmail.com>
2026-02-19 15:18:24 +0000

    Finished setting up

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,7 +2,7 @@ import os
 from block_markdown import markdown_to_html_node
 from pathlib import Path
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -18,7 +18,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
-
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
@@ -33,15 +34,15 @@ def extract_title(md):
             return line[2:]
     raise ValueError("no title found")
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     list_of_files = os.listdir(dir_path_content)
     for i in list_of_files:
         path_to_file = os.path.join(dir_path_content, i)
         if os.path.isfile(path_to_file) == True:
             dest_path_f = os.path.join(dest_dir_path, i)
             html_path = Path(dest_path_f).with_suffix(".html")
-            generate_page(path_to_file, template_path, html_path)
+            generate_page(path_to_file, template_path, html_path, basepath)
         elif os.path.isdir(path_to_file) == True:
             path_to_dir = os.path.join(dir_path_content, i)
             path_to_dest_dir = os.path.join(dest_dir_path, i)
-            generate_pages_recursive(path_to_dir, template_path, path_to_dest_dir)
+            generate_pages_recursive(path_to_dir, template_path, path_to_dest_dir, basepath)
